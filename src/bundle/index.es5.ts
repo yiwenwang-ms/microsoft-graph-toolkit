878a59053d87afac8197b6ff7e233eeef5bffae6
Nicolas Vogt <nicolasjvogt@gmail.com>
2020-04-14 12:57:31 -0700

    sharepoint working, still issues with mock

--- src/bundle/index.es5.ts
+++ src/bundle/index.es5.ts
@@ -5,9 +5,186 @@
  * -------------------------------------------------------------------------------------------
  */
 
-import 'core-js/stable';
+import '@webcomponents/webcomponentsjs/webcomponents-bundle';
+
+// minimum needed for sharepoint
+import 'core-js/features/string';
+import 'core-js/features/symbol';
+
 import 'regenerator-runtime/runtime';
 import 'whatwg-fetch';
-// tslint:disable-next-line: ordered-imports
-import '@webcomponents/webcomponentsjs/webcomponents-bundle';
+
+// minimum needed for localhost, causes issues with sharepoint (mgt-login)
+// import 'core-js/features/reflect';
+
+// minimum needed for localhost, causes issues with sharepoint (Console error only)
+// import 'core-js/features/weak-set';
+
+// fixes sharepoint "includes" causes issues with sharepoint + chromium + person-card
+// if (!String.prototype.includes) {
+//   String.prototype.includes = function(search, start) {
+//     'use strict';
+
+//     if (search instanceof RegExp) {
+//       throw TypeError('first argument must not be a RegExp');
+//     }
+//     if (start === undefined) {
+//       start = 0;
+//     }
+//     return this.indexOf(search, start) !== -1;
+//   };
+// }
+
+// https://tc39.github.io/ecma262/#sec-array.prototype.includes
+if (!Array.prototype.includes) {
+  Object.defineProperty(Array.prototype, 'includes', {
+    value(searchElement, fromIndex) {
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      // 1. Let O be ? ToObject(this value).
+      const o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      // tslint:disable-next-line: no-bitwise
+      const len = o.length >>> 0;
+
+      // 3. If len is 0, return false.
+      if (len === 0) {
+        return false;
+      }
+
+      // 4. Let n be ? ToInteger(fromIndex).
+      //    (If fromIndex is undefined, this step produces the value 0.)
+      // tslint:disable-next-line: no-bitwise
+      const n = fromIndex | 0;
+
+      // 5. If n ≥ 0, then
+      //  a. Let k be n.
+      // 6. Else n < 0,
+      //  a. Let k be len + n.
+      //  b. If k < 0, let k be 0.
+      let k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+      // tslint:disable-next-line: completed-docs
+      function sameValueZero(x, y) {
+        return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+      }
+
+      // 7. Repeat, while k < len
+      while (k < len) {
+        // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+        // b. If SameValueZero(searchElement, elementK) is true, return true.
+        if (sameValueZero(o[k], searchElement)) {
+          return true;
+        }
+        // c. Increase k by 1.
+        k++;
+      }
+
+      // 8. Return false
+      return false;
+    }
+  });
+}
+
+Number.isNaN =
+  Number.isNaN ||
+  function isNaN(input) {
+    return typeof input === 'number' && input !== input;
+  };
+
+if (typeof Object.assign !== 'function') {
+  // Must be writable: true, enumerable: false, configurable: true
+  Object.defineProperty(Object, 'assign', {
+    value: function assign(target, varArgs) {
+      // .length of function is 2
+      'use strict';
+      if (target === null || target === undefined) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+
+      const to = Object(target);
+
+      for (let index = 1; index < arguments.length; index++) {
+        const nextSource = arguments[index];
+
+        if (nextSource !== null && nextSource !== undefined) {
+          for (const nextKey in nextSource) {
+            // Avoid bugs when hasOwnProperty is shadowed
+            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+              to[nextKey] = nextSource[nextKey];
+            }
+          }
+        }
+      }
+      return to;
+    },
+    // tslint:disable-next-line: object-literal-sort-keys
+    configurable: true,
+    writable: true
+  });
+}
+
+if (!Object.entries) {
+  // tslint:disable-next-line: only-arrow-functions
+  Object.entries = function(obj) {
+    const ownProps = Object.keys(obj);
+    let i = ownProps.length;
+    // tslint:disable-next-line: prefer-const
+    let resArray = new Array(i); // preallocate the Array
+    while (i--) {
+      resArray[i] = [ownProps[i], obj[ownProps[i]]];
+    }
+    return resArray;
+  };
+}
+
+// https://tc39.github.io/ecma262/#sec-array.prototype.find
+if (!Array.prototype.find) {
+  Object.defineProperty(Array.prototype, 'find', {
+    value(predicate) {
+      // 1. Let O be ? ToObject(this value).
+      if (this == null) {
+        throw new TypeError('"this" is null or not defined');
+      }
+
+      const o = Object(this);
+
+      // 2. Let len be ? ToLength(? Get(O, "length")).
+      // tslint:disable-next-line: no-bitwise
+      const len = o.length >>> 0;
+
+      // 3. If IsCallable(predicate) is false, throw a TypeError exception.
+      if (typeof predicate !== 'function') {
+        throw new TypeError('predicate must be a function');
+      }
+
+      // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
+      const thisArg = arguments[1];
+
+      // 5. Let k be 0.
+      let k = 0;
+
+      // 6. Repeat, while k < len
+      while (k < len) {
+        // a. Let Pk be ! ToString(k).
+        // b. Let kValue be ? Get(O, Pk).
+        // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
+        // d. If testResult is true, return kValue.
+        const kValue = o[k];
+        if (predicate.call(thisArg, kValue, k, o)) {
+          return kValue;
+        }
+        // e. Increase k by 1.
+        k++;
+      }
+
+      // 7. Return undefined.
+      return undefined;
+    }
+  });
+}
+
 export * from './index.es6';
